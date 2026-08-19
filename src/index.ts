202fde29a23ae1ec2815be2df7a70de8e1e5c3e2
Sai Krishna <aketisaikrishna1@gmail.com>
2026-08-19 07:09:21 +0530

    Day 5 : Type alias practice

--- src/index.ts
+++ src/index.ts
@@ -174,143 +174,254 @@
 
 
 
-//DAY 4
+// //DAY 4
 
-//1
-const username = "krishna";
-const age =23;
-const email = "krishna@gmail.com";
-const isDeveloper = true;
-const city = "Hyderabad";
+// //1
+// const username = "krishna";
+// const age =23;
+// const email = "krishna@gmail.com";
+// const isDeveloper = true;
+// const city = "Hyderabad";
 
-//2
+// //2
 
-let userStatus : null | string  = null;
+// let userStatus : null | string  = null;
 
-let userStatus = "active";
-let userStatus = "inactive";
+// let userStatus = "active";
+// let userStatus = "inactive";
 
-//3
+// //3
 
-const product = {
-    id : 101,
-    name : "laptop",
-    price : 100000,
-    inStock : true
+// const product = {
+//     id : 101,
+//     name : "laptop",
+//     price : 100000,
+//     inStock : true
+// }
+
+// product.price = 200000;
+
+// console.log(product.price);
+
+// //4
+
+// const skills = ["JavaScript", "React", "TypeScript", "Node JS"];
+
+// skills.push('GASP'); //If you type number it rejects because number cannot be assigned to string array
+
+// //5
+
+// let phone1 : (number | string) = 987456321;
+//  phone1 = "123456789";
+//  //phone1 = true //not valid, because boolean cannot assigned to number or string
+// const email1 : string = "Krishna@gmail.com";
+// const website1 : string = "krsna.com";
+
+// //6
+
+// //tuples
+
+// let coordinates : [number , number] = [17.385, 78.534];
+// // ["17.385", 78.534]; Type 'string' is not assignable to type 'number'
+
+// // [17.385, 78.534, 100]; = 'coordinates' is declared but its value is never read.ts(6133)
+// // Type '[number, number, number]' is not assignable to type '[number, number]'.
+// //   Source has 3 element(s) but target allows only 2.
+
+
+// //7
+
+// const EmployeeData : {
+// name : string,
+// age : number,
+// salary : number,
+// isPermanent : boolean
+// } = {
+// name : "krsna",
+// age : 24,
+// salary : 100000,
+// isPermanent : true
+// }
+
+
+// const EmployeeData1 =  {
+// name : "krsna1",
+// age : 241,
+// salary : 1000001,
+// isPermanent : true
+// }
+
+// //8
+
+// const calculateSalary = (basicSalary : number , bonus : number) => {
+//         return basicSalary + bonus;
+// }
+
+// console.log(calculateSalary(12,8));
+
+// //9
+
+// let username3 = "Krishnaf";
+// let age3 = 66;
+// let email3 = "krishna@gmail.com";
+// let phone3: string | number = 987654321099;
+// let address3: string | null = "erhfb";
+// let skills3 = ["React", "TypeScript", "sfb"];
+// let isActive3 = true;
+
+// // Change username to another string. works
+// // Change age to another number. works
+// // Change phone from number to string. works
+// // Change address from null to a string. works
+// // Add another skill. works
+// // Try assigning a number to username. error
+// // Try assigning a boolean to phone. error
+// // Try assigning a number to address. error
+
+
+// //10
+
+// // Define role type explicitly
+// type UserRole = "admin" | "user";
+
+// // Define the interface for clarity
+// interface LoggedInUser {
+//   id: number;
+//   username: string;
+//   age: number;
+//   email: string;
+//   phone: string | number;   // union type
+//   role: UserRole;           // union type
+//   skills: string[];
+//   address: string | null;   // nullable
+//   isLoggedIn: boolean;
+// }
+
+// // Now create the object
+// const loggedin: LoggedInUser = {
+//   id: 1,                          // inferred as number
+//   username: "wgr",                // inferred as string
+//   age: 23,                        // inferred as number
+//   email: "Krsna@gmail.com",       // inferred as string
+//   phone: 43544563646,             // explicit union allows number
+//   role: "admin",                  // explicit union restricts to "admin" | "user"
+//   skills: ["one", "tei", "three"],// inferred as string[]
+//   address: null,                  // union allows null
+//   isLoggedIn: true                // inferred as boolean
+// };
+
+//Day 5
+
+type Address = {
+   readonly id : number,
+   houseNumber : number,
+   street : string,
+   city : string,
+   state : string,
+   country : string,
+   pincode : number,
+   landMark ?: string,
+   defaultAddress : boolean
 }
 
-product.price = 200000;
-
-console.log(product.price);
-
-//4
-
-const skills = ["JavaScript", "React", "TypeScript", "Node JS"];
-
-skills.push('GASP'); //If you type number it rejects because number cannot be assigned to string array
-
-//5
-
-let phone1 : (number | string) = 987456321;
- phone1 = "123456789";
- //phone1 = true //not valid, because boolean cannot assigned to number or string
-const email1 : string = "Krishna@gmail.com";
-const website1 : string = "krsna.com";
-
-//6
-
-//tuples
-
-let coordinates : [number , number] = [17.385, 78.534];
-// ["17.385", 78.534]; Type 'string' is not assignable to type 'number'
-
-// [17.385, 78.534, 100]; = 'coordinates' is declared but its value is never read.ts(6133)
-// Type '[number, number, number]' is not assignable to type '[number, number]'.
-//   Source has 3 element(s) but target allows only 2.
-
-
-//7
-
-const EmployeeData : {
-name : string,
-age : number,
-salary : number,
-isPermanent : boolean
-} = {
-name : "krsna",
-age : 24,
-salary : 100000,
-isPermanent : true
+type Company = {
+   readonly companyId : number,
+   companyName : string,
+   email : string,
+   phoneNumber : string,
+   website ?: string,
+   industry : string,
+   numberofEmployees : number,
+   companyAddress : Address,
+   companyStatus : "verified" | "NotVerified"
 }
 
 
-const EmployeeData1 =  {
-name : "krsna1",
-age : 241,
-salary : 1000001,
-isPermanent : true
+type User = {
+  readonly id : number,
+  firstName : string,
+  lastName : string,
+  email : string,
+  phoneNumber : string,
+  age : number,
+  accountStatus : boolean,
+  userRole : "admin" | "customer",
+  profileImage ?: string,
+  company ?: Company,      
+  multipleAddress : Address[],  
+  defaultAddress : Address,
+  accountCreationDate : number
 }
 
-//8
-
-const calculateSalary = (basicSalary : number , bonus : number) => {
-        return basicSalary + bonus;
+type ProductDimensions = {
+  height : number,
+  width : number,
+  depth : number
 }
 
-console.log(calculateSalary(12,8));
 
-//9
-
-let username3 = "Krishnaf";
-let age3 = 66;
-let email3 = "krishna@gmail.com";
-let phone3: string | number = 987654321099;
-let address3: string | null = "erhfb";
-let skills3 = ["React", "TypeScript", "sfb"];
-let isActive3 = true;
-
-// Change username to another string. works
-// Change age to another number. works
-// Change phone from number to string. works
-// Change address from null to a string. works
-// Add another skill. works
-// Try assigning a number to username. error
-// Try assigning a boolean to phone. error
-// Try assigning a number to address. error
-
-
-//10
-
-// Define role type explicitly
-type UserRole = "admin" | "user";
-
-// Define the interface for clarity
-interface LoggedInUser {
-  id: number;
-  username: string;
-  age: number;
-  email: string;
-  phone: string | number;   // union type
-  role: UserRole;           // union type
-  skills: string[];
-  address: string | null;   // nullable
-  isLoggedIn: boolean;
+type Product = {
+  readonly id : number,
+  productName : string,
+  description : string,
+  price : number,
+  discountPrice ?: number,
+  category : string,
+  brand: string,
+  stockquantity : number,
+  inStock : boolean,
+  images : string[],
+  company : Company,
+  productDimensions: ProductDimensions,
+  Productweight : number,
+  readonly SKU : string
 }
 
-// Now create the object
-const loggedin: LoggedInUser = {
-  id: 1,                          // inferred as number
-  username: "wgr",                // inferred as string
-  age: 23,                        // inferred as number
-  email: "Krsna@gmail.com",       // inferred as string
-  phone: 43544563646,             // explicit union allows number
-  role: "admin",                  // explicit union restricts to "admin" | "user"
-  skills: ["one", "tei", "three"],// inferred as string[]
-  address: null,                  // union allows null
-  isLoggedIn: true                // inferred as boolean
+type OrderItem = {
+  product: Product;
+  quantity: number;
+  priceAtTimeOfPurchase: number;
 };
 
 
+type Order = {
+    readonly id : number,
+    customer : User,
+    orderItem : OrderItem[],
+    shippingAddress : Address,
+    billingAddress : Address,
+    totalProductPrice : number,
+    discountAmount ?: number,
+    shippingCharge : number,
+    finalAmount : number,
+    paymentStatus : "Done" | "Pending",
+    orderStatus : "Delivered" | "NotDelivered",
+    OrderCreationData : string,
+    deliveryDate ?: string,
+    trackingNumber ?: number
+}
+
+
+type APIResponse1<User> = {
+   success : boolean,
+   message : string,
+   data : User[],
+   error ?: null | boolean
+}
+
+type APIResponse2<Product> = {
+   success : boolean,
+   message : string,
+   data : Product[],
+   error ?: null | boolean
+}
+
+type APIResponse3<Order> = {
+   success : boolean,
+   message : string,
+   data : Order[],
+   error ?: null | boolean
+}
 
 
 
